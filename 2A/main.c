a237666669b186c9d67eae5e5a42ae789a01d029
ilogin <1107450@gmail.com>
2020-01-22 21:14:10 -0800

    Add first attempts at a, d, e, f, g, i in part a

--- 2A/main.c
+++ 2A/main.c
@@ -25,6 +25,56 @@ void PWM_Init() {
 	GPIOE->PUPDR &= ~GPIO_PUPDR_PUPD8;
 	
 	// Configure PWM Output for TIM1 CH 1N
+	AFR[0] &= ~GPIO_AFRL_AFSEL1;
+	AFR[0] |= GPIO_AFRL_AFSEL1_0;
+
+	//a: Set direction s.t. timer counts up
+	TIM1_CR1 &= ~TIM_CR1_DIR;
+
+
+	//b: Set the prescaler value (TO-DO)
+
+
+	//c: Set the auto-reload value (TO-DO)
+
+
+
+	//d: Configure channel 1 to be in output compare mode
+	//d(i): Clear output compare mode bits for channel 1
+	TIM1_CCMR1 &= ~TIM_CCMR1_OC1M;
+	
+	//d(ii): Set the output compare mode bits to PWM mode 1 (0110)
+	TIM1_CCMR1 |= TIM_CCMR1_OC1M_1;
+	TIM1_CCMR1 |= TIM_CCMR1_OC1M_2;
+	
+	//d(iii): Enable the output preload for channel 1
+	TIM1_CCMR1 |= TIM_CCMR1_OC1PE;
+
+
+
+	//e: Set the output polarity for channel 1N to active high
+	TIM1_CCER &= ~TIM_CCER_CC1P;
+
+
+
+	//f: Enable the output for channel 1N (TO-DO)
+	TIM1_CCER |= TIM_CCER_CC1E;
+
+
+	//g: Enable the main output
+	TIM1_BDTR |= TIM_BDTR_MOE;
+
+
+
+	//h: Set capture/compare value. (TO-DO)
+
+
+
+	//i: Enable the counter
+	TIM1_CR1 |= TIM_CR1_CEN;
+
+
+
 
 }
  
